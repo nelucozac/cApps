dc1955e9f96ea53188c02b1e10c42daa392b8026
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-01-27 11:54:19 +0200

    Create noSession.c

---
+++ main/noSession.c
@@ -0,0 +1,12 @@
+/*
+ License GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+ This is free software: you are free to change and redistribute it.
+ Dummy functions to supply the lack of session support.
+ See attached documentation for details.
+*/
+
+#include "cAppserver.h"
+
+void initSessions(char *Sfn) { }
+
+void writeSession(SRV_conn *Conn) { }
