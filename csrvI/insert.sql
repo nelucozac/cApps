e2f2c9e6f1b4ae1832f9ec01ceecd5138ff39cb1
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-01-29 09:34:53 +0200

    Create insert.sql

---
+++ csrvI/insert.sql
@@ -0,0 +1,6 @@
+insert into Dictio (wrd,wno,def) values
+('program',1,'[ Late Latin programma, from Greek ] : a public notice'),
+('program',2,'a plan or system under which action may be taken toward a goal'),
+('system',1,'a regularly interacting or interdependent group of items forming a unified whole'),
+('system',2,'an organized set of doctrines, ideas, or principles usually intended to explain the arrangement or working of a systematic whole'),
+('system',3,'harmonious arrangement or pattern ; order')
