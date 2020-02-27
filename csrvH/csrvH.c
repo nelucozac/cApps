94ba4864546a790d8c2d495876de247f3b8b0a59
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-02-27 10:18:46 +0200

    Update csrvH.c

--- csrvH/csrvH.c
+++ csrvH/csrvH.c
@@ -10,12 +10,12 @@ static struct {
        int fLog, lAdr;
        } Config;
 
-static void recordRequest(SRV_conn *Conn) {
+static void recordRequest(CAS_srvconn_t *Conn) {
 char *Bf,*Pb,*P,*Q;
 int l;
 Bf = calloc(65536,1);
-inet_ntop(Srvinfo.af,Conn->Ipc,Bf,INET6_ADDRSTRLEN);
-Pb = endOfString(Bf,0);
+inet_ntop(CAS_Srvinfo.af,Conn->Ipc,Bf,INET6_ADDRSTRLEN);
+Pb = CAS_endOfString(Bf,0);
 *Pb++ = ' ';
 for (P=Conn->Bfi+5; *P; P++)
     if (isspace(*P)) break;
@@ -25,7 +25,7 @@ Pb += l;
 *Pb++ = '\n';
 if (P=strcasestr(P,"User-agent:")) {
    Q = strpbrk(P,"\r\n");
-   if (Q==NULL) Q = endOfString(P,0);
+   if (Q==NULL) Q = CAS_endOfString(P,0);
    l = Q - P;
    memcpy(Pb,P,l);
    Pb += l;
@@ -42,17 +42,17 @@ return memcmp(Config.Ipn,Ipc,Config.lAdr) != 0;
 
 static void userConfig(char *Cfg) {
 char *P;
-Config.lAdr = Srvinfo.af == AF_INET ? 4 : 16;
+Config.lAdr = CAS_Srvinfo.af == AF_INET ? 4 : 16;
 for (P=Cfg; *P; P++)
     if (isspace(*P)) break;
 *P++ = 0;
-inet_pton(Srvinfo.af,Cfg,Config.Ipn);
+inet_pton(CAS_Srvinfo.af,Cfg,Config.Ipn);
 while (isspace(*P)) P++;
 Cfg = P;
 /* other user specific configuration data */
 }
 
-static char *concat(SRV_conn *Conn, ...) {
+static char *concat(CAS_srvconn_t *Conn, ...) {
 va_list Prms;
 char *Pch,*Out;
 int l;
@@ -83,37 +83,37 @@ while (c=*P) {
 return Str;
 }
 
-static void manageTimeout(SRV_conn *Conn) {
+static void manageTimeout(CAS_srvconn_t *Conn) {
 double s,w;
 long long p,n;
-n = atoll(getLastParamValue(Conn,"N"));
+n = atoll(CAS_getLastParamValue(Conn,"N"));
 if (n==0) n = 9000000000;
-nPrintf(Conn,"<br>Heavy program sequence begins, n = %D<br>",n);
+CAS_nPrintf(Conn,"<br>Heavy program sequence begins, n = %D<br>",n);
 for (s=0,w=0.25,p=1; p<=n; p++,w=-w) {
     s += w * p;
     if (p%1000==0) if (Conn->tmo) break;
     }
-nPrintf(Conn,"%s s = %.2f, p = %D<br>",Conn->tmo?"Partial":"Completed",s,p);
+CAS_nPrintf(Conn,"%s s = %.2f, p = %D<br>",Conn->tmo?"Partial":"Completed",s,p);
 Conn->tmo = 0;
-nPrintf(Conn,"Elapsed time: %.3e",getTime(NULL)-Conn->tim);
+CAS_nPrintf(Conn,"Elapsed time: %.3e",CAS_getTime(NULL)-Conn->tim);
 }
 
-static void processRequest(SRV_conn *Conn) {
+static void processRequest(CAS_srvconn_t *Conn) {
 char *A,*B,*C,*S;
 A = "string A";
 B = "string B";
 C = "string C";
 S = " - ";
-nPrintf(Conn,"First concatenation: %s<br>",concat(Conn,A,S,B,S,C,NULL));
-nPrintf(Conn,"Second concatenation: %s<br>",concat(Conn,B,S,C,A,NULL));
-nPrintf(Conn,"%s<br>",stringToUpper(cPrintf(Conn,"%x",0xabcdef)));
+CAS_nPrintf(Conn,"First concatenation: %s<br>",concat(Conn,A,S,B,S,C,NULL));
+CAS_nPrintf(Conn,"Second concatenation: %s<br>",concat(Conn,B,S,C,A,NULL));
+CAS_nPrintf(Conn,"%s<br>",stringToUpper(CAS_sPrintf(Conn,"%x",0xabcdef)));
 manageTimeout(Conn);
 }
 
-void registerUserSettings(void) {
+void CAS_registerUserSettings(void) {
 Config.fLog = open("ssrvH.log",O_APPEND|O_CREAT|O_WRONLY|O_DSYNC,0600);
-Srvinfo.rwrl = recordRequest;
-Srvinfo.preq = processRequest;
-Srvinfo.cnfg = userConfig;
-Srvinfo.acco = acceptConnection;
+CAS_Srvinfo.rwrl = recordRequest;
+CAS_Srvinfo.preq = processRequest;
+CAS_Srvinfo.cnfg = userConfig;
+CAS_Srvinfo.acco = acceptConnection;
 }
