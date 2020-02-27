a04a2315866379ceb72507e34c1f3d0ffe82bb59
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-02-27 10:16:32 +0200

    Update datsG.h

--- csrvG/datsG.h
+++ csrvG/datsG.h
@@ -9,19 +9,19 @@
 
 #define _dat_tG_h
 
-typedef struct { int k; char *N; } INF_node;
+typedef struct { int k; char *N; } T_nodeinf;
 
-typedef struct { int s, c; } INF_succ;
+typedef struct { int s, c; } T_succinf;
 
 typedef struct {
         int nn; /* number of graph nodes */
         int ml; /* maximum length of a node name */
-        INF_node *In; /* key and name of nodes from 1 to nn - In[1], In[2] etc */
-        INF_node **Ia; /* list nodes, alphabetically ordered - Ia[0], Ia[1] etc */
-        INF_succ **Sc; /* lists of successors and costs */
-        } INF_graph;
+        T_nodeinf *In; /* key and name of nodes from 1 to nn - In[1], In[2] etc */
+        T_nodeinf **Ia; /* list nodes, alphabetically ordered - Ia[0], Ia[1] etc */
+        T_succinf **Sc; /* lists of successors and costs */
+        } T_graphinf;
 
-extern INF_graph Graph;
+extern T_graphinf Graph;
 
 typedef struct {
         int *C; /* costs of paths from dep to x */
@@ -29,9 +29,9 @@ typedef struct {
         int *Q; /* priority queue / nodes defining the path */
         int n;  /* number of elements in Q */
         signed char *E; /* -1 element not yet inserted in Q, 0 inserted, 1 removed */
-        } VMARK;
+        } T_vmark;
 
-void minCostPath(int dep, int arv, VMARK *Mk),
+void minCostPath(int dep, int arv, T_vmark *Mk),
      manageUserData(char op);
 
 #endif
