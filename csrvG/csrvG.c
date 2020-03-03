bdf43d3506aa4f9e942505891381b33defe4321d
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-03-03 09:27:38 +0200

    csrvG module

--- csrvG/csrvG.c
+++ csrvG/csrvG.c
@@ -2,7 +2,7 @@
  License GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
  This is free software: you are free to change and redistribute it.
 */
-//
+
 #include "datsG.h"
 
 typedef struct { char *Fh, *D, *A; } T_userinf;
