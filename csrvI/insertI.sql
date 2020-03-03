2ead55f685191486d8f31a4c88298c52bfc9716b
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-03-03 09:49:50 +0200

    Update insertI.sql

--- csrvI/insertI.sql
+++ csrvI/insertI.sql
@@ -3,4 +3,4 @@ insert into Dictio (wrd,wno,def) values
 ('program',2,'a plan or system under which action may be taken toward a goal'),
 ('system',1,'a regularly interacting or interdependent group of items forming a unified whole'),
 ('system',2,'an organized set of doctrines, ideas, or principles usually intended to explain the arrangement or working of a systematic whole'),
-('system',3,'harmonious arrangement or pattern ; order')
+('system',3,'harmonious arrangement or pattern ; order');
