0d3d78c7c6056006374ff292f2b45df92f511d4e
nelucozac <60288337+nelucozac@users.noreply.github.com>
2020-03-03 09:52:07 +0200

    Compile script for csrvI (release)

--- csrvI/cmplsI.sh
+++ csrvI/cmplsI.sh
@@ -1,4 +1,3 @@
 gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c `mysql_config --cflags` csrvI.c `mysql_config --libs` -lpthread -lm -o csrvI
 chmod 700 csrvI
 strip csrvI
-;
