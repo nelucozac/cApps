21195399379c975177898256481ce093d1572121
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:45:20 +0200

    Compile script for csrvD (release)

--- csrvD/cmplsD.sh
+++ csrvD/cmplsD.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvD.c -o csrvD
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvD.c -lpthread -o csrvD
 chmod 700 csrvD
 strip csrvD
