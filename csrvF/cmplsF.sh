633259ed1375df963440394b4c3154ea4979bd32
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:47:14 +0200

    Compile script for csrvF (release)

--- csrvF/cmplsF.sh
+++ csrvF/cmplsF.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvF.c -o csrvF
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvF.c -lpthread -o csrvF
 chmod 700 csrvF
 strip csrvF
