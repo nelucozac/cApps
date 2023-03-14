047947fb956d6b011d29aa99300b4a553bd336f7
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:44:37 +0200

    Compile script for csrvB (release)

--- csrvB/cmplsB.sh
+++ csrvB/cmplsB.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvB.c -o -lpthread csrvB
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvB.c -lpthread -o csrvB
 chmod 700 csrvB
 strip csrvB
