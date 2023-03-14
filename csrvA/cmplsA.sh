49b8291bebabeefd148461530818365989a783ac
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:41:25 +0200

    Compile script for csrvA (release)

--- csrvA/cmplsA.sh
+++ csrvA/cmplsA.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvA.c -o csrvA
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvA.c -lpthread -o csrvA
 chmod 700 csrvA
 strip csrvA
