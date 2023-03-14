a1dcbc39258581be66a532acd610ee289d8dd315
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:54:08 +0200

    Functions for graph data management

--- csrvG/datsG.c
+++ csrvG/datsG.c
@@ -10,8 +10,6 @@
 
 T_graphinf Graph;
 
-static int32_t mtx = 1;
-
 static char *myScanf(char *Bfi, char *Fmt, ...) {
 va_list Prms;
 char *Pch,c;
@@ -132,12 +130,10 @@ return 1;
 void minCostPath(int dep, int arv, T_vmark *Mk) {
 int x,y,w,*C;
 T_succinf *Ps,*Pn;
-CAS_serverMutex(NULL,&mtx,'L');
 Mk->C = calloc(Graph.nn+1,sizeof(int));
 Mk->P = calloc(Graph.nn+1,sizeof(int));
 Mk->Q = calloc(Graph.nn+1,sizeof(int));
 Mk->E = malloc(Graph.nn+1);
-CAS_serverMutex(NULL,&mtx,'R');
 for (x=1,C=Mk->C+1; x<=Graph.nn; x++)
     *C++ = Infinity;
 memset(Mk->E,-1,Graph.nn+1);
