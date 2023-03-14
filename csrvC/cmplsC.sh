9e7636c1dab968e0bb0f11e62ec12ddfa41b6958
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:43:11 +0200

    Compile script for csrvC (release)

--- csrvC/cmplsC.sh
+++ csrvC/cmplsC.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvC.c -o csrvC
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvC.c -lpthread -o csrvC
 chmod 700 csrvC
 strip csrvC
