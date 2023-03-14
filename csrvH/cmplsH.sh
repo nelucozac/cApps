c8075442b8d2ca5ead253ff61b3100e28e3bf60b
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-03-14 14:54:47 +0200

    Compile script for csrvH (release)

--- csrvH/cmplsH.sh
+++ csrvH/cmplsH.sh
@@ -1,3 +1,3 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvH.c -o csrvH
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvH.c -lpthread -o csrvH
 chmod 700 csrvH
 strip csrvH
