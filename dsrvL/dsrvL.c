f13ce9b389f0ba25b423d89007d26d91b12ccb27
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-05-29 10:44:09 +0300

    Twelfth app (protection against slowloris post attack)
    Twelfth app (protection against slowloris post attack)

--- dsrvL/dsrvL.c
+++ dsrvL/dsrvL.c
@@ -96,7 +96,7 @@ Ussn->etm = Conn->uts + 3600;
 static void processRequest(CAS_srvconn_t *Conn) {
 char *Opt, *Sva, *Msg;
 int k;
-Opt = CAS_getLastParamValue(Conn,"Option");
+Opt = CAS_getLastParamValue(Conn,"Action");
 if (strcmp(Opt,"Login")==0) {
    k = checkIfLoginOk(Conn);
    switch (k) {
