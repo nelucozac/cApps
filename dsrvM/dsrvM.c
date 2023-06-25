aac20c771f81287bc544096a356aceda82768d61
nelucozac <60288337+nelucozac@users.noreply.github.com>
2023-06-25 16:12:05 +0300

    Thirteenth app (how to upload file on server)

---
+++ dsrvM/dsrvM.c
@@ -0,0 +1,24 @@
+#include "cAppserver.h"
+
+static char *Htm;
+
+static void manageHtmlFiles(char op) {
+if (op=='L') Htm = CAS_loadTextFile("Upload.htm");
+   else free(Htm);
+}
+
+static void processRequest(CAS_srvconn_t *Conn) {
+char *Fnam;
+if (Conn->Bfi[0]=='L') {
+   Fnam = CAS_getLastParamValue(Conn,"fname");
+   rename(Conn->Ufn,Fnam);
+   CAS_nPrintf(Conn,"%s uploaded",Fnam);
+   return;
+   }
+CAS_nPrintf(Conn,Htm,CAS_Srvinfo.fs,CAS_Srvinfo.fs);
+}
+
+void CAS_registerUserSettings(void) {
+CAS_Srvinfo.preq = processRequest;
+CAS_Srvinfo.html = manageHtmlFiles;
+}
