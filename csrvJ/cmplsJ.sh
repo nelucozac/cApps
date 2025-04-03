07e1b436dbcbc625dfdd2dea5084245c57fcb1fc
nelucozac <60288337+nelucozac@users.noreply.github.com>
2025-04-03 15:12:46 +0300

    Delete csrvJ directory

--- csrvJ/cmplsJ.sh
+++
@@ -1,3 +0,0 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/noSession.c csrvJ.c -lpthread -o csrvJ
-chmod 700 csrvJ
-strip csrvJ
--- csrvJ/dbgsJ.sh
+++
@@ -1,2 +0,0 @@
-gcc -g -I../main/ ../main/cAppserver.c ../main/noSession.c csrvJ.c -lpthread -o csrvJ
-chmod 700 csrvJ
