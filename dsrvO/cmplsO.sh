824e7de22c0c655a19cb8392d01ae645cecf9fce
nelucozac <60288337+nelucozac@users.noreply.github.com>
2025-04-17 14:52:57 +0300

    Delete dsrvO directory

--- dsrvO/cmplsO.sh
+++
@@ -1,3 +0,0 @@
-gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/flSession.c dsrvO.c -lpthread -o dsrvO
-chmod 700 dsrvO
-strip dsrvO
--- dsrvO/dbgsO.sh
+++
@@ -1,2 +0,0 @@
-gcc -g -I../main/ ../main/cAppserver.c ../main/flSession.c dsrvO.c -lpthread -o dsrvO
-chmod 700 dsrvO
