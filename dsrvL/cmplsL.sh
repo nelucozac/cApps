1aab3441e25f70b92f4a9a55e9ce0cd114eb86fe
nelucozac <60288337+nelucozac@users.noreply.github.com>
2025-04-25 11:08:43 +0300

    Compile script for dsrvL and dPlus (release)

--- dsrvL/cmplsL.sh
+++ dsrvL/cmplsL.sh
@@ -1,3 +1,4 @@
 gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/flSession.c dsrvL.c -lpthread -o dsrvL
-chmod 700 dsrvL
-strip dsrvL
+gcc -O3 -D _Release_application_server -I../main/ ../main/cAppserver.c ../main/flSession.c dPlus.c -lpthread -o dPlus
+chmod 700 dsrvL dPlus
+strip dsrvL dPlus
