e715bff473f44621239368318d8c762b78eaa840
nelucozac <60288337+nelucozac@users.noreply.github.com>
2025-07-06 17:42:22 +0300

    Compile script for csrvG (debug)

---
+++ csrvG/dbgsG.sh
@@ -0,0 +1,2 @@
+gcc -g -I../main/ ../main/cAppserver.c ../main/noSession.c csrvG.c -lpthread -o csrvG
+chmod 700 csrvG
--- csrvH/dbgsH.sh
+++
@@ -1,2 +0,0 @@
-gcc -g -I../main/ ../main/cAppserver.c ../main/noSession.c csrvH.c -lpthread -o csrvH
-chmod 700 csrvH
