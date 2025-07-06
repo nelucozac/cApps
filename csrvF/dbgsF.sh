b9248235883a6ca8271bb4031c10dcb62042ee00
nelucozac <60288337+nelucozac@users.noreply.github.com>
2025-07-06 17:37:48 +0300

    Compile script for csrvF (debug)

---
+++ csrvF/dbgsF.sh
@@ -0,0 +1,2 @@
+gcc -g -I../main/ ../main/cAppserver.c ../main/noSession.c csrvF.c datsF.c -lpthread -o csrvF
+chmod 700 csrvF
--- csrvG/dbgsG.sh
+++
@@ -1,2 +0,0 @@
-gcc -g -I../main/ ../main/cAppserver.c ../main/noSession.c csrvG.c datsG.c -lpthread -o csrvG
-chmod 700 csrvG
